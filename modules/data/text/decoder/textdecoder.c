4cd28e84c267f41e0c84789f465d7021153a9f27
Kris Kowal <kris@cixar.com>
2021-12-29 17:34:40 -0800

    fix build error for textdecoder

--- modules/data/text/decoder/textdecoder.c
+++ modules/data/text/decoder/textdecoder.c
@@ -398,10 +398,13 @@ void modInstallTextDecoder(xsMachine *the)
 
 	xsVar(kScratch) = xsNewHostFunction(xs_textdecoder_decode, 1);
 	xsmcSet(xsVar(kPrototype), xsID("decode"), xsVar(kScratch));
+
 	xsVar(kScratch) = xsNewHostFunction(xs_textdecoder_get_encoding, 0);
 	xsmcDefine(xsVar(kPrototype), xsID("encoding"), xsVar(kScratch), xsIsGetter);
+
 	xsVar(kScratch) = xsNewHostFunction(xs_textdecoder_get_ignoreBOM, 0);
 	xsmcDefine(xsVar(kPrototype), xsID("ignoreBOM"), xsVar(kScratch), xsIsGetter);
+
 	xsVar(kScratch) = xsNewHostFunction(xs_textdecoder_get_fatal, 0);
 	xsmcDefine(xsVar(kPrototype), xsID("fatal"), xsVar(kScratch), xsIsGetter);
 
