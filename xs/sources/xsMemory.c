defb44522751344008780d7c717b32305ea1e022
Mathieu Hofman <mathieu@agoric.com>
2022-09-18 13:47:48 +0000

    fix: zero init trailing chunk memory

--- xs/sources/xsMemory.c
+++ xs/sources/xsMemory.c
@@ -1435,6 +1435,9 @@ void* fxRenewChunk(txMachine* the, void* theData, txSize size)
 				aBlock->current += delta;
 				aChunk->temporary = aBlock->current;
 				aChunk->size = size;
+			#ifdef mxSnapshot
+				c_memset(aData + capacity, 0, delta);
+			#endif
 			#ifdef mxNever
 				gxRenewChunkCases[1]++;
 			#endif
